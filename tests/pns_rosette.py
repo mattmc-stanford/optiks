633bf9413514c1dced491d51a360c8a52e778d63
mattmc-stanford <mattmc@stanford.edu>
2025-01-21 19:30:25 -0800

    Creating git repo for OPTIKS

---
+++ tests/pns_rosette.py
@@ -0,0 +1,45 @@
+from src.optiks.optiks import optiks
+from src.optiks.loss_functions import *
+import matplotlib
+matplotlib.use("webagg")
+from scipy.io import savemat
+from src.optiks.options import *
+from src.optiks.utils import rosetteTraj
+
+# Setting hardware options==============================================================================================
+hw = HardwareOpts(gfin=0, gmax=10, smax=19.5, dt=4e-3)
+
+# Designing desired trajectory (Rosette)================================================================================
+res = 0.1  # cm^-1
+C = rosetteTraj(res, n1=7, n2=5, npoints=800)
+C_m = np.hstack((np.real(C), np.imag(C))).astype(float)
+
+# Setting design options================================================================================================
+Pthresh = 100*0.8
+system = "UHP"
+if system == "MAGNUS":
+    r = 52.2
+    c = 611e-6
+    alpha = 0.324
+elif system == "UHP":
+    r = 26.5
+    c = 359e-6
+    alpha = 0.37
+else:
+    r = 23.4
+    c = 334e-6
+    alpha = 0.333
+params = {'terms': [time_min, slew_lim, pns_lim],
+          'pns': [Pthresh, r, c, alpha]}
+weights = {'time': 1e4,
+           'slew': 7e2,
+           'pns': 5e1}
+des = DesignOpts(params=params, weights=weights)
+
+# Setting solver options================================================================================================
+sv = SolverOpts(ds=5e-5, maxiter=500, count=50, derate=0.9)
+
+# Designing gradient waveforms==========================================================================================
+C_v, t_sf, g_sf, s_sf, g_usf, s_usf, g_last = optiks(C_m, hwopts=hw, dsopts=des, svopts=sv)
+
+savemat('../Waveforms/current_run.mat', {'gsafe': g_sf, 'gunsafe': g_usf, 'trajectory': C_v, 'glast': g_last}, format='4')
